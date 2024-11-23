fe6bc97ae87bd1463cc86d8d84a170c554543a5e
StefaniaKoulouri <t8230063@aueb.gr>
2024-11-23 19:03:28 +0200

    Update Mainview.java

--- src/main/Mainview.java
+++ src/main/Mainview.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,14 +12,22 @@ public class Mainview extends Application {
     @Override
     public void start(Stage primaryStage) {
       
-    	Button enterButton = new Button("Είσοδος στην εφαρμογή");
+    	Label titleLabel = new Label("Βελτιστοποιήση Ραντεβού Ιατρείου");
+        titleLabel.setStyle("-fx-font-size: 36px; -fx-font-weight: bold; -fx-text-fill: #2E4057;");
+        
+        Label descriptionLabel = new Label("(Περιγραφή εφαρμογής)");
+        descriptionLabel.setStyle("-fx-font-size: 18px; -fx-text-fill: #4A4A4A;");
+        
+        Button enterButton = new Button("Είσοδος στην εφαρμογή");
         enterButton.setStyle("-fx-background-color: #D8A7D2; -fx-text-fill: black; -fx-font-size: 20px;");
         enterButton.setPrefSize(250, 60);
         
-        VBox vbox = new VBox(30); // 
+        VBox vbox = new VBox(80); 
         vbox.setStyle("-fx-background-color: #A8D0E6;"); 
         vbox.setAlignment(Pos.CENTER); 
+        vbox.setPadding(new Insets(50, 20, 20, 20))
         
+        VBox.setMargin(titleLabel, new Insets(-250, 0, 0, 0));
         vbox.getChildren().addAll(enterButton);
         
         Scene scene = new Scene(vbox, 1000, 700); 
