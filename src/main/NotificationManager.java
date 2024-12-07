2e0815c8a15435740a6a917d18b15c944a412d13
Thodoris Taloumis <t8220145@aueb.gr>
2024-12-07 02:45:29 +0200

    Update NotificationManager.java

--- src/main/NotificationManager.java
+++ src/main/NotificationManager.java
@@ -6,13 +6,15 @@ public class NotificationManager {
 
     public void notifyDoctor(Doctor doctor, List<Appointment> schedule) {
         System.out.println("Notifying " + doctor.getFullname() + " of their schedule:");
-        schedule.forEach(
-                appointment -> System.out
-                        .println("  - " + appointment.getDateTime() + " - " + appointment.getDetails()));
+        schedule.stream()
+                .filter(appointment -> appointment.getDoctor().equals(doctor))
+                .forEach(appointment -> System.out.println("  - " + appointment));
     }
 
-    public void notifyPatient(Patient patient, Appointment appointment) {
-        System.out.println("Reminder for " + patient.getName() + ": Your appointment is on " +
-                appointment.getDateTime() + " with Dr. " + appointment.getDoctor().getFullname());
+    public void notifyPatient(Patient patient, List<Appointment> schedule) {
+        System.out.println("Notifying " + patient.getName() + " of their appointments:");
+        schedule.stream()
+            .filter(appointment -> appointment.getDetails().contains(patient.getName()))
+            .forEach(appointment -> System.out.println("  - " + appointment));
     }
 }
