f4ab09237dcbb7534ebc8ab714432ca18abeb61a
zoee-g <t8230023@aueb.gr>
2024-12-13 19:51:55 +0200

    Update IsNewPatient.java

--- src/main/IsNewPatient.java
+++ src/main/IsNewPatient.java
@@ -3,4 +3,12 @@ import java.awt.*;
 
 public class IsNewPatient extends JFrame {
 
+    public IsNewPatient() {
+
+        setTitle("Στοιχεία Ασθενή");
+        setSize(2000, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+  
 }
