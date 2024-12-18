cd92577ca67c01b7b9efac84b565d54cdd2c6937
StefaniaKoulouri <t8230063@aueb.gr>
2024-12-18 15:01:01 +0200

    Update AppointmentInfo.java

--- src/main/AppointmentInfo.java
+++ src/main/AppointmentInfo.java
@@ -17,9 +17,24 @@ public class AppointmentInfo extends JFrame {
         
         selectAppointment();
         selectDateAndTime();
+        addButton();
     }
 
+private void addButton() {
+        JButton submitButton = new JButton("Συνέχεια");
+        submitButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        submitButton.setBackground(new Color(173, 216, 230));
+        submitButton.setForeground(Color.BLACK);
+        submitButton.setPreferredSize(new Dimension(120, 50));
 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.insets = new Insets(100, 20, 20, 20); 
+        gbc.anchor = GridBagConstraints.CENTER; 
+        add(submitButton, gbc); 
+    }
 public static void main(String[] args) {
         DocInfo frame = new DocInfo(); 
         frame.setVisible(true);        
