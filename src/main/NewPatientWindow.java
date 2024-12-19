49df88932b4a6cc065c36809f15363475c6fdd13
zoee-g <t8230023@aueb.gr>
2024-12-19 21:17:56 +0200

    Update NewPatientWindow.java

--- src/main/NewPatientWindow.java
+++ src/main/NewPatientWindow.java
@@ -7,13 +7,30 @@ public class NewPatientWindow extends JFrame {
     private static final int LABEL_FONT_SIZE = 22;
     private static final int FIELD_WIDTH = 500;
     private static final int FIELD_HEIGHT = 30;
+    private String selectedDate;
+    private String selectedTime;
     
-    public NewPatientWindow() {
+    public NewPatientWindow(String date, String time) {
+
+        this.selectedDate = date;
+        this.selectedTime = time;
 
         setTitle("Στοιχεία Ασθενή");
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Color lightBlue = new Color(230, 245, 255);
+        getContentPane().setBackground(lightBlue);
 
+        JPanel mainPanel = createMainPanel(lightBlue);
+        add(mainPanel, BorderLayout.CENTER);
+
+        JPanel bottomPanel = createBottomPanel(lightBlue);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+        
     }
+    
   
 }
