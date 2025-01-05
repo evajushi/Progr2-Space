6646fd3c686f8d357fd3fa03d28f3c2163696138
StefaniaKoulouri <t8230063@aueb.gr>
2025-01-05 21:34:41 +0200

    Update AppointmentWelcomeScreenTest.java

--- src/test/AppointmentWelcomeScreenTest.java
+++ src/test/AppointmentWelcomeScreenTest.java
@@ -1,1 +1,19 @@
+package space;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class AppointmentWelcomeScreenTest {
+  private AppointmentWelcomeScreen window;
+
+    @Before
+    public void setUp() {
+        window = new AppointmentWelcomeScreen();
+    }
+
+}
